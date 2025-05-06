407e40c10f10ac50f89880769f2e4ab028936e7c
baxodirovfarxod <baxodirovfarxod@2007.com>
2025-05-06 22:05:57 +0500

    Prepared solution structure with required folders and NuGet packages

---
+++ src/GameStore/GameStore.Api/Configurations/DependencyInjectionConfigurations.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.Api.Configurations;
+
+public static class DependencyInjectionConfigurations
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+
+    }
+}
---
+++ src/GameStore/GameStore.Dal/MainContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Dal;
+
+public class MainContext : DbContext
+{
+    public MainContext(DbContextOptions<MainContext> options)
+      : base(options)
+    { 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MainContext).Assembly);
+    }
+}

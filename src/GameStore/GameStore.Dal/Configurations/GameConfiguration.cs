a1aeead74c88ce3c31f6f285c43f0ef3a6bd622f
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-05-07 23:17:24 +0500

    Create Game entity and configure relationships using Fluent API

---
+++ src/GameStore/GameStore.Dal/Configurations/GameConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using GameStore.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameStore.Dal.Configurations;
+
+public class GameConfiguration : IEntityTypeConfiguration<Game>
+{
+    public void Configure(EntityTypeBuilder<Game> builder)
+    {
+        builder.ToTable("Games");
+
+        builder.HasKey(g => g.Id);
+
+        builder.Property(g => g.Id)
+            .IsRequired();
+
+        builder.Property(g => g.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(g => g.Key)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(g => g.Key)
+            .IsUnique();
+
+        builder.Property(g => g.Description)
+            .HasMaxLength(500);
+    }
+}
+
--- src/GameStore/GameStore.Dal/MainContext.cs
+++ src/GameStore/GameStore.Dal/MainContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GameStore.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Dal;
 
@@ -6,8 +7,11 @@ public class MainContext : DbContext
 {
     public MainContext(DbContextOptions<MainContext> options)
       : base(options)
-    { 
+    {
     }
+
+    public DbSet<Game> Games { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

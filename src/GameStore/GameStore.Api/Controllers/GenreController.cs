a99d04db3cfd82f8006c92a384befddcfea5fa1b
baxodirovfarxod <baxodirovfarxod@2007.com>
2025-05-09 20:11:58 +0500

    Enable response caching for GET endpoints to improve performance

--- src/GameStore/GameStore.Api/Controllers/GenreController.cs
+++ src/GameStore/GameStore.Api/Controllers/GenreController.cs
@@ -22,24 +22,28 @@ namespace GameStore.Api.Controllers
         }
 
         [HttpGet("getById/{id}")]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
         public async Task<GenreDto> GetGenreById(Guid id)
         {
             return await genreService.GetByIdAsync(id);
         }
 
         [HttpGet("getAll")]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
         public async Task<List<GenreGetDto>> GetAllGenres()
         {
             return await genreService.GetAllAsync();
         }
 
         [HttpGet("getByGameKey/{key}")]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
         public async Task<List<GenreGetDto>> GetByGameKeyEndpoint(string key)
         {
             return await genreService.GetByGameKeyAsync(key);
         }
 
         [HttpGet("getByParentId/{id}")]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
         public async Task<List<GenreGetDto>> GetByParentId(Guid id)
         {
             return await genreService.GetByParentIdAsync(id);

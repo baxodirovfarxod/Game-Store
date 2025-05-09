0533193cc6c4c2e01101f44be0e39bd1db2ae735
Kamron Noriyev <kamronnoriyev7@gmail.com>
2025-05-09 11:07:36 +0500

    Create GamePlatform entity with composite key and foreign key relationships

--- src/GameStore/GameStore.Dal/Entities/Game.cs
+++ src/GameStore/GameStore.Dal/Entities/Game.cs
@@ -6,5 +6,6 @@ public class Game
     public string Name { get; set; }
     public string Key { get; set; }
     public string? Description { get; set; }
+    public ICollection<GamePlatform> GamePlatforms { get; set; } 
 }
 

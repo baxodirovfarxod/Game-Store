3021395bbcbad723010d10571ed047aa81e8054b
Kamron Noriyev <kamronnoriyev7@gmail.com>
2025-05-09 22:20:51 +0500

    ok

--- src/GameStore/GameStore.Bll/Validators/GameValidators.cs
+++ src/GameStore/GameStore.Bll/Validators/GameValidators.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation;
+using GameStore.Bll.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,18 @@ using System.Threading.Tasks;
 
 namespace GameStore.Bll.Validators
 {
-   public class GameValidators 
+    // Assuming the correct DTO class is GameDto within the Bll.Dtos namespace
+    public class GameValidators : AbstractValidator<GameDto>
     {
-
+        public GameValidators()
+        {
+            RuleFor(game => game.Name)
+                .NotEmpty()
+                .WithMessage("Game name is required.")
+                .Length(3, 50)
+                .WithMessage("Game name must be between 1 and 100 characters.");
+        }
+    }
 
     }
 }
